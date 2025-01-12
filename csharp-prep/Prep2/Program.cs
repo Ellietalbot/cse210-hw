d032689712d2c813518f8a20e72210ede234478b
Ellietalbot <tal20011@byui.edu>
2025-01-11 21:18:59 -0700

    did prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade_percentage = Console.ReadLine();
+        int percentage = int.Parse(grade_percentage);
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80){
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage>=60){
+            letter = "D";
+        }
+        else{
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (percentage <= 70)
+        {
+            Console.WriteLine("Keep Trying!");
+        }
+        else
+        {
+            Console.WriteLine("Great job! You passed the class!");
+        }
     }
 }
