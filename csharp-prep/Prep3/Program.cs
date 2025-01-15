66ccad9078927ea7d07368d390688591a5d81b12
Ellietalbot <tal20011@byui.edu>
2025-01-15 10:52:04 -0700

    did exercises

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int random_int = random.Next(1,101);
+        int guess = -1;
+
+        while (guess != random_int)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess > random_int){
+                Console.WriteLine("Lower");
+            }
+            else if (guess < random_int){
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
+
