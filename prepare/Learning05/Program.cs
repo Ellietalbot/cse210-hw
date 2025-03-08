fecb835339c19396063f1798a398c4b6663358ec
Ellietalbot <tal20011@byui.edu>
2025-03-08 11:41:39 -0700

    did learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Yellow", 9);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Red", 8.8, 12);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Orange", 78.2);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine(color);
+            Console.WriteLine(area);
+
+        }
+
+
     }
 }
